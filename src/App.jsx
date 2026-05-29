51003e3a4601b28e4ca812d5dbad216552de845b
karim <karimjaber09@gmail.com>
2026-05-30 02:28:12 +0300

    Switch to HashRouter to fix page refresh 404 and disappearing images

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,29 @@ import Team from "./pages/Team";
 import ContactUs from "./pages/ContactUs";
 import LoginPage from "./pages/LoginPage";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 
 function App() {
-
-const [user,setUser] = useState("guest")
+  const [user, setUser] = useState("guest");
 
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Nav user={user} />
         <Routes>
           <Route path="*" element={<Home />} />
-          <Route path="/Home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/Pricing" element={<Pricing />} />
           <Route path="/Team" element={<Team />} />
           <Route path="/Contact" element={<ContactUs />} />
-          <Route path="/Login" element={<LoginPage user={user} setUser={setUser} />} />
+          <Route
+            path="/Login"
+            element={<LoginPage user={user} setUser={setUser} />}
+          />
         </Routes>
         <Footer />
-      </BrowserRouter>
-
-      <div></div>
+      </HashRouter>
     </>
   );
 }
